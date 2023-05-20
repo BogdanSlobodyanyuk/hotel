efc586fa63cb24275e711050a02a3c09e770b009
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2023-05-20 15:07:47 +0100

    initial commit

--- src/main/java/com/robot/hotel/HotelApplication.java
+++ src/main/java/com/robot/hotel/HotelApplication.java
@@ -1,13 +1,12 @@
 package com.robot.hotel;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
 
 @SpringBootApplication
 public class HotelApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(HotelApplication.class, args);
-    }
 
+    }
 }
